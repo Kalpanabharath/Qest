adfa0c3cbb3c8c10ca371827170cefe6591c9278
kalpana Bharath <stonepanakeerthu@gmail.com>
2024-12-21 20:33:59 +0530

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="Qest/" element={<Homepage />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/solutions" element={<Support />} />
       </Routes>
